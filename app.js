765dd1194780907b4852c2d6bec7ca7237ec4cdf
Jayesh Choudhary <choudharyjayesh668@gmail.com>
2026-05-18 19:07:50 +0530

    Complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
+const User=require ("./models/user.js");
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://127.0.0.1:27017/test")
+mongoose.connect("mongodb://127.0.0.1:27017/LoginAuthentication")
     .then(() => {
         console.log("MongoDB Connected");
     })
@@ -22,8 +23,40 @@ mongoose.connect("mongodb://127.0.0.1:27017/test")
         console.log(err);
     });
 app.get("/", (req, res) => {
-    res.send("Server Working");
+    res.render("signup.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs", {
+        error: null
+    });
+})
+
+app.post("/signup", async (req, res) => {
+    let {email,password}=req.body;
+    const newUser= new User({
+        email,
+        password
+    });
+    await newUser.save();
+    res.redirect("/login");
+});
+
+app.post("/login",async (req,res)=>{
+   let {email,password}=req.body;
+   const foundUser= await User.findOne({email:email});
+   if(!foundUser){
+       return res.render("login.ejs", {
+           error: "Email not found"
+       });
+   }
+    if(foundUser.password !== password){
+        return res.render("login.ejs", {
+            error: "Password does not match"
+        });
+   }
+   return res.render("homepage.ejs");
 });
 app.listen(8080, () => {
-    console.log("Server is listening on port 8080");
+    console.log("http://localhost:8080/");
 });
